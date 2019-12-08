d20159334eaa6677dde2eb4b5b1d43e7538eddc7
Viktoriia Beznisko <viktoriiabeznisko@gmail.com>
2019-12-08 05:32:10 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,16 +1,16 @@
-# class Application
+class Application
  
-#   def call(env)
-#     resp = Rack::Response.new
-#     req = Rack::Request.new(env)
+  def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
  
-#     if req.path=="/songs"
-#       resp.write "You requested the songs"
-#     else
-#       resp.write "Route not found"
-#       resp.status = 404
-#     end
+    if req.path=="/songs"
+      resp.write "You requested the songs"
+    else
+      resp.write "Route not found"
+      resp.status = 404
+    end
  
-#     resp.finish
-#   end
-# end
+    resp.finish
+  end
+end

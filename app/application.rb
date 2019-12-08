f8eed009da1634f47672a0dc1701b7b46995152e
Viktoriia Beznisko <viktoriiabeznisko@gmail.com>
2019-12-08 05:33:35 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -8,9 +8,19 @@ class Application
       resp.write "You requested the songs"
     else
       resp.write "Route not found"
-      resp.status = 404
+      resp.status = 400
     end
  
     resp.finish
   end
 end
+
+
+ 
+    if req.path.match(/songs/)
+ 
+      song_title = req.path.split("/songs/").last #turn /songs/Sorry into Sorry
+      song = @@songs.find{|s| s.title == song_title}
+ 
+      resp.write song.artist
+    end
